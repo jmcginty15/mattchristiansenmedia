feea6f6d1244efca9fb901a3b951fd516fffbf2d
Jason McGinty <jason_mcginty@yahoo.com>
2021-11-07 15:19:13 -0600

    removing api key

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const PORT = process.env.PORT || 3001;
 const DATING_SHEET_ID = '1DrWstDsCJwPgU9BxYKGnCqvU_iZ9KpPgcroAyNLimtA';
 const MEETUP_SHEET_ID = '1gMjDg4WpowclIkvb-T1rt9yssBui4GMIydvFZWaaPb8';
-// const API_KEY = process.env.API_KEY;
-const API_KEY = 'AIzaSyAur5RGEjAwe9wQ5_remV4Rs5H32sz8xqM';
+const API_KEY = process.env.API_KEY;
 
 module.exports = { PORT, API_KEY, DATING_SHEET_ID, MEETUP_SHEET_ID };
